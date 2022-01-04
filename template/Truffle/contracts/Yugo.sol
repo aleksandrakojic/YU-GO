d9703477258e76f9ea4a1900fdf36c1ac6fbda74
unknown <zynk123@hotmail.com>
2022-01-04 01:24:24 +0100

    yugo token + tallyVote

--- template/Truffle/contracts/Yugo.sol
+++ template/Truffle/contracts/Yugo.sol
@@ -3,8 +3,9 @@ pragma solidity 0.8.11;
 
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
+contract Token is ERC20, Ownable {
 
 
     constructor () ERC20("Yugo", "YG") {
