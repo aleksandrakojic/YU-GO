81c549635cba532dcda4a2f15f0a3640d73d7286
Aleksandra K <alexandra.kojic@gmail.com>
2022-01-06 08:47:09 +0100

    re-install , landing page

--- Truffle/scripts/deployContract.js
+++ Truffle/scripts/deployContract.js
@@ -2,14 +2,17 @@
 const { spawn } = require('child_process');
 
 const run = async () => {
-  console.log('📄 Deploying and updating contracts...');
-  try {
-    spawn('cd Truffle && truffle migrate --reset --compile-all --network ropsten && node scripts/contractInfo.js', {
-      shell: true,
-      stdio: 'inherit'
-    });
-  } catch (e) {
-    console.log(e);
-  }
+	console.log('📄 Deploying and updating contracts...');
+	try {
+		spawn(
+			'cd Truffle && truffle migrate --reset --compile-all --network develop && node scripts/contractInfo.js',
+			{
+				shell: true,
+				stdio: 'inherit',
+			}
+		);
+	} catch (e) {
+		console.log(e);
+	}
 };
 run();
